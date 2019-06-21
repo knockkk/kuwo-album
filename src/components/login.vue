78565dc613efc38955fe007b48461c438075d0bd
JARYGAME <184354994@qq.com>
2019-06-21 09:49:08 +0800

    login

--- src/components/login.vue
+++ src/components/login.vue
@@ -1,29 +1,84 @@
-<template>
-    <div>
-        <h1>Welcome to kuwoAlbum</h1>
-        <el-button @click="loginClick">登录</el-button>
-    </div>
+ 
+<template id="registTemp">
+   <el-form :model="formData" :rules="rules" ref="formData" label-position="left" label-width="0px" class="demo-ruleForm login-container">
+    <h3 class="title">Welcome to KuWoAlbum</h3>
+    
+    <el-form-item prop="account">
+      <el-input type="text"  v-model="formData.account" placeholder="用户名"></el-input>
+    </el-form-item>
+    <el-form-item prop="checkPass">
+      <el-input type="password"  v-model="formData.checkPass"  placeholder="密码"></el-input>
+    </el-form-item>
+    <el-form-item>
+    <el-link type="primary" style="margin-right:20px">忘记密码</el-link>
+   <el-link type="primary">注册</el-link>
+   </el-form-item>
+    
+    <el-form-item style="width:100%;">
+      <el-button type="primary" style="width:100%;">登录</el-button>
+    </el-form-item>
+
+  </el-form>
 </template>
+
 
 <script>
 import { Url } from '../config/index';
 export default {
     data() {
         return {
-
+        formData: {
+          account: '',
+          checkPass: ''
+        },
+        rules: {
+          account: [
+            { required: true, message: '请输入账号', trigger: 'blur' },
+            //{ validator: validaePass }
+          ],
+          checkPass: [
+            { required: true, message: '请输入密码', trigger: 'blur' },
+            //{ validator: validaePass2 }
+          ]
+        },
         }
+       
     },
     methods: {
         loginClick() {
              console.log("login")
-             this.$router.push(Url.indexPage);
+             this.$router.push(Url.loginPage);
         }
     }
 }
 </script>
-
 <style scoped>
-
-</style>
+  .login-container {
+    /*box-shadow: 0 0px 8px 0 rgba(0, 0, 0, 0.06), 0 1px 0px 0 rgba(0, 0, 0, 0.02);*/
+    -webkit-border-radius: 5px;
+    border-radius: 5px;
+    -moz-border-radius: 5px;
+    background-clip: padding-box;
+    margin: 180px auto;
+    width: 350px;
+    padding: 35px 35px 15px 35px;
+    background: #fff;
+    border: 1px solid #eaeaea;
+    box-shadow: 0 0 15px #cac6c6;
+    }
+    .title {
+      margin: 0px auto 40px auto;
+      text-align: center;
+      color: #505458;
+    }
+.remember {
+      margin: 0px 0px 35px 0px;
+    }
+    .right {
+    position: absolute;
+    right: 0px;
+    
+}
+  
 </style>
 
